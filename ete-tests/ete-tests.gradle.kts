f37c487f8c378c01585e0b8393deb07762dc59b5
Callum Rogers <callumrogers@hotmail.co.uk>
2016-12-04 11:52:39 +0000

    Start circle build

--- ete-tests/ete-tests.gradle.kts
+++ ete-tests/ete-tests.gradle.kts
@@ -1,4 +1,8 @@
+
+import org.gradle.api.Task
+import org.gradle.api.tasks.Exec
 import org.gradle.script.lang.kotlin.dependencies
+import org.gradle.script.lang.kotlin.task
 import org.gradle.script.lang.kotlin.testCompile
 
 buildscript {
@@ -28,3 +32,33 @@ dependencies {
     testCompile("ch.qos.logback:logback-classic:1.1.7")
     testCompile("com.palantir.docker.compose:docker-compose-rule:0.28.1")
 }
+
+val buildFrontend = task<Exec>("buildFrontend") {
+    setExecutable("./webpack")
+    setWorkingDir("../frontend")
+}
+
+val openReports = task<Exec>("openReports") {
+    setExecutable("open")
+    setArgs(listOf("target/site/serenity/build-info.html"))
+}
+
+val test: Task = tasks.getByName("test")
+val clearReports: Task = tasks.getByName("clearReports")
+val aggregate: Task = tasks.getByName("aggregate")
+val checkOutcomes: Task = tasks.getByName("checkOutcomes")
+
+task("etes") {
+    dependsOn(
+        test,
+        aggregate,
+        checkOutcomes
+    )
+}
+
+test.dependsOn(clearReports)
+test.dependsOn(buildFrontend)
+test.outputs.upToDateWhen { false }
+
+aggregate.mustRunAfter(test)
+checkOutcomes.mustRunAfter(aggregate)
