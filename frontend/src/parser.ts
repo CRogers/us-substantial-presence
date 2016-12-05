7099a4aa05b2ea2eb281fa542ec898094603c5f9
Callum Rogers <callumrogers@hotmail.co.uk>
2016-12-05 15:40:45 +0000

    Start parsing

--- frontend/src/parser.ts
+++ frontend/src/parser.ts
@@ -1,10 +1,12 @@
+import * as _ from 'lodash'
+
 export module UsSubPres.Parser {
     export interface TravelHistory {
         trips: Trip[];
     }
 
     export interface Trip {
-        enter: PortVisit;
+        entry: PortVisit;
         exit: PortVisit;
     }
 
@@ -15,9 +17,40 @@ export module UsSubPres.Parser {
 
     export type Port = string;
 
-    export function parseTravelHistory(blah: string): TravelHistory {
+    export function parseTravelHistory(str: string): TravelHistory {
+        if (str.length == 0) {
+            return { trips: [] }
+        }
+
+        let nonEmptyLine = (line: string) => !/^\s*$/.test(line);
+
+        let trimmedLines = _(str)
+            .split('\n')
+            .filter(nonEmptyLine)
+            .map(line => line.trim())
+            .value();
+
+        let trip = _.drop(trimmedLines, 1);
+
         return {
-            trips: []
+            trips: [parseTrip(trip)]
+        }
+    }
+
+    function parseTrip(trip: string[]): Trip {
+        let entry = _.take(trip, 2);
+        let exit = _.drop(trip, 2);
+
+        return {
+            entry: parsePortVisit(entry),
+            exit: parsePortVisit(exit)
+        }
+    }
+
+    function parsePortVisit(portVisit: string[]): PortVisit {
+        return {
+            time: new Date(portVisit[0]),
+            port: portVisit[1]
         }
     }
 }
--- frontend/test/parserTest.ts
+++ frontend/test/parserTest.ts
@@ -1,14 +1,55 @@
 import 'mocha'
 import { expect } from 'chai'
 
+import * as _ from 'lodash'
+
 import * as ParserM from '../src/parser'
 let Parser = ParserM.UsSubPres.Parser;
 
 module UsSubPres.Tests {
     describe('Parser should', () => {
-        it('parse the empty string to no files', () => {
+        it('parse the empty string to no trips', () => {
             let travelHistory = Parser.parseTravelHistory('');
             expect(travelHistory).deep.equal({trips: []})
+        });
+
+        it('parse a single trip', () => {
+            let rawText = stripIndent(`
+                1\t
+                2016-09-12T11:55:54.0-04:00
+                JFK - JOHN F KENNEDY INTL\t
+                2016-09-16T00:00:00.0-04:00
+                NYC - NEW YORK CITY, NY
+            `);
+
+            expect(Parser.parseTravelHistory(rawText)).to.deep.equal({
+                trips: [
+                    {
+                        entry: {
+                            port: 'JFK - JOHN F KENNEDY INTL',
+                            time: new Date('2016-09-12T11:55:54.0-04:00')
+                        },
+                        exit: {
+                            port: 'NYC - NEW YORK CITY, NY',
+                            time: new Date('2016-09-16T00:00:00.0-04:00')
+                        }
+                    }
+
+                ]
+            })
         })
     });
+
+    function stripIndent(rawText: string) {
+        let lines = rawText.split('\n');
+        let minIndent: number = _(lines)
+            .filter(line => line.length > 0 && !(/^\s+$/.test(line)))
+            .map(line => line.length - line.replace(/^\s+/, '').length)
+            .reduce((a, b) => Math.min(a, b), Number.MAX_VALUE);
+        let trimmedLines = _(lines)
+            .map(line => line.substring(minIndent))
+            .value();
+        let overall = trimmedLines.join('\n');
+        return overall;
+    }
 }
