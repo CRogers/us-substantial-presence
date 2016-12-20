cd9be5293a022727b5ab616c94abc57361360610
Callum Rogers <callumrogers@hotmail.co.uk>
2016-12-20 18:09:23 +0000

    Fix deploy script

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu
+set -exu
 
 git config user.email "circleci-build-node@circleci.com"
 git config user.name "CircleCI build node"
@@ -9,9 +9,9 @@ HASH_MESSAGE="$(git show --oneline | head -n1)"
 
 git checkout --orphan gh-pages
 
-cp frontend/site/ site/
+cp -r frontend/site/ site/
 
-echo * | tr -clearReports " " "\n" | grep -v -E "site|circle\.yml" | xargs rm -rf
+echo * | tr -s " " "\n" | grep -v -E "site|circle\.yml" | xargs rm -rf
 cp site/* .
 rm -rf site/
 
