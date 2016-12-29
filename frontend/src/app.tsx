5466450037ad1b34c880d22922cb2694ec5e306a
Callum Rogers <callumrogers@hotmail.co.uk>
2016-12-29 23:43:00 +0000

    Add chart

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,5 +1,6 @@
 import '@blueprintjs/core/dist/blueprint.css'
 import '@blueprintjs/datetime/dist/blueprint-datetime.css'
+import 'plottable/plottable.css'
 import {DatePicker} from '@blueprintjs/datetime'
 import './app.less'
 
@@ -13,6 +14,7 @@ import * as _ from 'lodash'
 import * as Parser from './parser'
 import * as Calculator from './calculator'
 import {Optional} from "./optional";
+import {AdjustedDays} from "./parser";
 
 function renderTravelHistoryTable(travelHistory: Parser.TravelHistory, date: moment.Moment) {
     let rows = _.map(travelHistory.trips, trip =>
@@ -45,14 +47,14 @@ function renderTravelHistoryTable(travelHistory: Parser.TravelHistory, date: mom
 }
 
 
+
 module UsSubPres.UI {
     type JsxStream = Rx.Observable<JSX.Element>;
 
     let container = document.createElement('div');
     document.body.appendChild(container);
 
-    let travelHistoryEvents: Rx.Subject<React.FormEvent<HTMLTextAreaElement>> = new Rx.Subject<React.FormEvent<HTMLTextAreaElement>>();
-    travelHistoryEvents.subscribe(value => console.log(value));
+    let travelHistoryEvents = new Rx.Subject<React.FormEvent<HTMLTextAreaElement>>();
 
     // let travelHistoryText: Rx.Observable<string> = Rx.Observable.of(defaultTravelHistory);
     let travelHistoryText: Rx.Observable<string> = travelHistoryEvents.map(ev => (ev.target as HTMLTextAreaElement).value).startWith('');
@@ -85,6 +87,50 @@ module UsSubPres.UI {
 
     let detailsUI: JsxStream = detailsTable.map(detailsTableOrError => detailsTableOrError.orElse(<div>Cannot render table</div>));
 
+    interface TimePlot {
+        date: moment.Moment;
+        value: AdjustedDays;
+    }
+
+    let graphUI = (() => {
+        const xScale = new Plottable.Scales.Time();
+        const xAxis = new Plottable.Axes.Time(xScale, 'bottom');
+
+        const yScale = new Plottable.Scales.Linear();
+        const yAxis = new Plottable.Axes.Numeric(yScale, 'left');
+
+        const dataset = new Plottable.Dataset();
+
+        parsedTravelHistory.subscribe(travelHistoryOrError => {
+            const data = travelHistoryOrError.map(travelHistory => {
+                const now = moment();
+                const threeYearsAgo = moment(moment().year(), 'YYYY').subtract(3, 'years');
+
+                const returnData: TimePlot[] = [];
+                for (let date = now; date.isAfter(threeYearsAgo); date = date.clone().subtract('1', 'day')) {
+                    returnData.push({date, value: travelHistory.adjustedDaysUpTo(date)});
+                }
+                return returnData;
+            }).orElse([]);
+
+            console.log(data);
+
+            dataset.data(data);
+        });
+
+        const lineRenderer = new Plottable.Plots.Line<Date>()
+                .x(d => d.date.toDate(), xScale)
+                .y(d => d.value, yScale)
+            .addDataset(dataset);
+
+        const chart = new Plottable.Components.Table([
+            [yAxis, lineRenderer],
+            [null, xAxis]
+        ] as Plottable.Component[][]);
+
+        return <div style={{width: '1000px', height: '400px'}}><svg ref={element => chart.renderTo(element)}></svg></div>;
+    })();
+
     let wholeUI: JsxStream = Rx.Observable.combineLatest(testResultUI, detailsUI, (testResultElement, detailsElement) =>
         <div>
             {travelHistoryUI}
@@ -92,6 +138,7 @@ module UsSubPres.UI {
             <h2>Details</h2>
             {detailsElement}
             <DatePicker defaultValue={new Date()} onChange={(date, blah) => selectedDateSubject.onNext(date)}></DatePicker>
+            {graphUI}
         </div>
     );
 
